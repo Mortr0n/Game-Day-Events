d2f856e135fc25e9769c1d7864ad466642b5bbcc
Mortron <cmsl4y3r@gmail.com>
2022-01-19 20:13:03 -0700

    Login Register and logreg mostly functional

--- Game_Day/server/routes/user.routes.js
+++ Game_Day/server/routes/user.routes.js
@@ -2,9 +2,9 @@ const userController = require('../controllers/user.controller');
 const { authenticate } = require('../config/jwt.config');
 
 module.exports = (app) => {
-    app.post("/api/register", userController.register);
-    app.post("/api/login", userController.login);
-    app.post("/api/logout", userController.logout);
+    app.post("/api/users/register", userController.register);
+    app.post("/api/users/login", userController.login);
+    app.post("/api/users/logout", userController.logout);
     // this route now has to be authenticated
     app.get("/api/users",  userController.getAll);
 }
